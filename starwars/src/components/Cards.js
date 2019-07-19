605b1c69925101a876c467614644596f98dd4fa2
Neha Patel <neha.r.p@gmail.com>
2019-07-19 16:18:00 -0700

    added character data to cards

--- starwars/src/components/Cards.js
+++ starwars/src/components/Cards.js
@@ -2,8 +2,18 @@ import React from "react";
 
 function Cards(props) {
     console.log(props)
+    const { name, height, mass, hairColor, skinColor, eyeColor } = props
 
-    return Cards;
+    return (
+        <div>
+            <h1>{name}</h1>
+            <p><span>Height: </span>{height}</p>
+            <p><span>Mass: </span>{mass}</p>
+            <p><span>Hair Color: </span>{hairColor}</p>
+            <p><span>Skin Color:</span>{skinColor}</p>
+            <p><span>Eye Color:</span>{eyeColor}</p>
+        </div>
+    )
 }
 
 export default Cards;
